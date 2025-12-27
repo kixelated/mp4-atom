37997900fdc41bed281dbd4ce0577c328b142846
Brad Hards <bradh@frogmouth.net>
2025-12-27 13:09:14 +1100

    mp4a: fix taic encoding
    Thanks @coderabbitai for flagging this

--- src/moov/trak/mdia/minf/stbl/stsd/mp4a/mod.rs
+++ src/moov/trak/mdia/minf/stbl/stsd/mp4a/mod.rs
@@ -47,6 +47,9 @@ impl Atom for Mp4a {
         if self.btrt.is_some() {
             self.btrt.encode(buf)?;
         }
+        if self.taic.is_some() {
+            self.taic.encode(buf)?;
+        }
 
         Ok(())
     }
@@ -90,6 +93,50 @@ mod tests {
                 avg_bitrate: 3,
             }),
             taic: None,
+        };
+        let mut buf = Vec::new();
+        expected.encode(&mut buf).unwrap();
+
+        let mut buf = buf.as_ref();
+        let decoded = Mp4a::decode(&mut buf).unwrap();
+        assert_eq!(decoded, expected);
+    }
+
+    #[test]
+    fn test_mp4a_with_taic() {
+        let expected = Mp4a {
+            audio: Audio {
+                data_reference_index: 1,
+                channel_count: 2,
+                sample_size: 16,
+                sample_rate: 48000.into(),
+            },
+            esds: Esds {
+                es_desc: esds::EsDescriptor {
+                    es_id: 2,
+                    dec_config: esds::DecoderConfig {
+                        object_type_indication: 0x40,
+                        stream_type: 0x05,
+                        up_stream: 0,
+                        buffer_size_db: Default::default(),
+                        max_bitrate: 67695,
+                        avg_bitrate: 67695,
+                        dec_specific: esds::DecoderSpecific {
+                            profile: 2,
+                            freq_index: 4,
+                            chan_conf: 2,
+                        },
+                    },
+                    sl_config: esds::SLConfig::default(),
+                },
+            },
+            btrt: None,
+            taic: Some(Taic {
+                time_uncertainty: 1,
+                clock_resolution: 2,
+                clock_drift_rate: 4,
+                clock_type: ClockType::CanSync,
+            }),
         };
         let mut buf = Vec::new();
         expected.encode(&mut buf).unwrap();
