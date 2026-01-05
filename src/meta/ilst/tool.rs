22c5a23ab1490baf8584af79756abb5144d2c42e
Brad Hards <bradh@frogmouth.net>
2026-01-05 16:55:36 +1100

    updates from code review

--- src/meta/ilst/tool.rs
+++ src/meta/ilst/tool.rs
@@ -48,14 +48,14 @@ impl Atom for Tool {
                 let type_indicator = u32::decode(buf)?;
                 if type_indicator != TYPE_INDICATOR_UTF8 {
                     return Err(Error::Unsupported(
-                        "Only UTF-8 text is support in ilst tool box",
+                        "Only UTF-8 text is supported in ilst tool box",
                     ));
                 }
                 let country_indicator = u16::decode(buf)?;
                 let language_indicator = u16::decode(buf)?;
-                let remaining_bytes = (type_indicator_or_len - 16) as usize;
+                let remaining_bytes = buf.remaining();
                 let body = &mut buf.slice(remaining_bytes);
-                let text = String::from_utf8(body.to_vec()).unwrap();
+                let text = String::from_utf8(body.to_vec()).map_err(|_| Error::InvalidSize)?;
                 buf.advance(remaining_bytes);
                 Ok(Tool {
                     country_indicator,
@@ -68,6 +68,10 @@ impl Atom for Tool {
 
     fn encode_body<B: BufMut>(&self, buf: &mut B) -> Result<()> {
         let text_bytes = self.text.as_bytes();
+        // the length of the nested atom is the length field (4 bytes),
+        // the 4CC (4 bytes), the type indicator (4 bytes), the country
+        // indicator (2 bytes), the language indicator (2 bytes) and
+        // then the actual text.
         let nested_len = (4 + 4 + 4 + 2 + 2 + text_bytes.len()) as u32;
         nested_len.encode(buf)?;
         DATA_4CC.encode(buf)?;
@@ -103,6 +107,7 @@ mod tests {
         b'a', 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, b'L', b'a', b'v', b'f', b'6', b'1',
         b'.', b'7', b'.', b'1', b'0', b'0',
     ];
+
     #[test]
     fn test_ilst_tool_long_style() {
         let inbuf: &mut std::io::Cursor<&&[u8]> = &mut std::io::Cursor::new(&ENCODED_CTOO);
@@ -117,5 +122,43 @@ mod tests {
         ctoo.encode(&mut outbuf).unwrap();
 
         assert_eq!(outbuf.as_slice(), ENCODED_CTOO);
+    }
+
+    #[test]
+    fn test_bad_nested_atom() {
+        let buf = vec![
+            0x00, 0x00, 0x00, 28, 0xA9, 0x74, 0x6F, 0x6F, 0x00, 0x00, 0x00, 0x1C, b'd', b'a', b't',
+            b'x', 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, b't', b'e', b's', b't',
+        ];
+
+        let parse_result = Tool::decode(&mut buf.as_slice());
+        assert!(parse_result.is_err());
+        match parse_result.err().unwrap() {
+            Error::UnexpectedBox(four_cc) => {
+                assert_eq!(four_cc, FourCC::new(b"datx"));
+            }
+            _ => {
+                panic!("unexpected error");
+            }
+        }
+    }
+
+    #[test]
+    fn test_bad_type_indicator() {
+        let buf = vec![
+            0x00, 0x00, 0x00, 28, 0xA9, 0x74, 0x6F, 0x6F, 0x00, 0x00, 0x00, 0x1C, b'd', b'a', b't',
+            b'a', 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, b't', b'e', b's', b't',
+        ];
+
+        let parse_result = Tool::decode(&mut buf.as_slice());
+        assert!(parse_result.is_err());
+        match parse_result.err().unwrap() {
+            Error::Unsupported(s) => {
+                assert_eq!(s, "Only UTF-8 text is supported in ilst tool box")
+            }
+            _ => {
+                panic!("unexpected error");
+            }
+        }
     }
 }
