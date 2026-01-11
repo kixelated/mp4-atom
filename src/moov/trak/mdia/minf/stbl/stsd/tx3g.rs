bb202677e5ee1beba5dcbd41b4d00abf00064cb4
Brad Hards <bradh@frogmouth.net>
2026-01-11 15:59:55 +1100

    tx3g: implement ftab box
    This comes up in a couple of the MPEG file format conformance files as
    the tx3g codec under decoding.
    
    See 3GPP TS 26.245 or ETSI TS 126 245 Section 5.1
    
    Its not clear that the box has to be there, so made it an Option.

--- src/moov/trak/mdia/minf/stbl/stsd/tx3g.rs
+++ src/moov/trak/mdia/minf/stbl/stsd/tx3g.rs
@@ -1,5 +1,9 @@
 use crate::*;
 
+/// Text track sample description format for tx3g
+///
+/// 3GPP TS 26.245 or ETSI TS 126 245 Section 5.16
+/// See https://www.etsi.org/deliver/etsi_ts/126200_126299/126245/18.00.00_60/ts_126245v180000p.pdf
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Tx3g {
@@ -10,6 +14,9 @@ pub struct Tx3g {
     pub bg_color_rgba: RgbaColor,
     pub box_record: [i16; 4],
     pub style_record: [u8; 12],
+    // Looks like this is supposed to be present, but we're relaxed about it.
+    pub ftab: Option<Ftab>,
+    // TODO: possibly there is a default disparity box here too, but never seen.
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
@@ -36,6 +43,7 @@ impl Default for Tx3g {
             },
             box_record: [0, 0, 0, 0],
             style_record: [0, 0, 0, 0, 0, 1, 0, 16, 255, 255, 255, 255],
+            ftab: None, // TODO: possibly a nice Serif?
         }
     }
 }
@@ -64,6 +72,13 @@ impl Atom for Tx3g {
             i16::decode(buf)?,
         ];
         let style_record = <[u8; 12]>::decode(buf)?;
+        let mut ftab = None; // TODO
+        while let Some(atom) = Any::decode_maybe(buf)? {
+            match atom {
+                Any::Ftab(atom) => ftab = atom.into(),
+                unknown => Self::decode_unknown(&unknown)?,
+            }
+        }
 
         Ok(Tx3g {
             data_reference_index,
@@ -73,6 +88,7 @@ impl Atom for Tx3g {
             bg_color_rgba,
             box_record,
             style_record,
+            ftab,
         })
     }
 
@@ -93,6 +109,7 @@ impl Atom for Tx3g {
         for n in 0..12 {
             (self.style_record[n]).encode(buf)?;
         }
+        self.ftab.encode(buf)?;
 
         Ok(())
     }
@@ -117,6 +134,7 @@ mod tests {
             },
             box_record: [0, 0, 0, 0],
             style_record: [0, 0, 0, 0, 0, 1, 0, 16, 255, 255, 255, 255],
+            ftab: None,
         };
         let mut buf = Vec::new();
         expected.encode(&mut buf).unwrap();
@@ -124,5 +142,102 @@ mod tests {
         let mut buf = buf.as_ref();
         let decoded = Tx3g::decode(&mut buf).unwrap();
         assert_eq!(decoded, expected);
+    }
+
+    // From the MPEG file format conformance test suite: isobmff/09_text.mp4
+    const ENCODED_TX3G_09: &[u8] = &[
+        0x00, 0x00, 0x00, 0x40, 0x74, 0x78, 0x33, 0x67, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x01, 0x00, 0x04, 0x00, 0x00, 0x01, 0xff, 0xff, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x3c, 0x01, 0x90, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x12, 0xff, 0xff, 0xff,
+        0xff, 0x00, 0x00, 0x00, 0x12, 0x66, 0x74, 0x61, 0x62, 0x00, 0x01, 0x00, 0x01, 0x05, 0x53,
+        0x65, 0x72, 0x69, 0x66,
+    ];
+
+    /* From isobmff/09_text_gpac.json:
+           "@Size": "64",
+           "@Type": "tx3g",
+           "@Specification": "3gpp",
+           "@Container": "stsd",
+           "@dataReferenceIndex": "1",
+           "@displayFlags": "40000",
+           "@horizontal-justification": "1",
+           "@vertical-justification": "-1",
+           "@backgroundColor": "ff 0 0 ff",
+           "DefaultBox": {
+               "BoxRecord": {
+               "@top": "0",
+               "@left": "0",
+               "@bottom": "60",
+               "@right": "400"
+               },
+               "FontTableBox": {
+               "@Size": "18",
+               "@Type": "ftab",
+               "@Specification": "3gpp",
+               "@Container": "tx3g text enct",
+               "FontRecord": {
+                   "@ID": "1",
+                   "@name": "Serif"
+               }
+               }
+           },
+           "DefaultStyle": {
+               "StyleRecord": {
+               "@startChar": "0",
+               "@endChar": "0",
+               "@fontID": "1",
+               "@styles": "Normal",
+               "@fontSize": "18",
+               "@textColor": "ff ff ff ff"
+               }
+           },
+           "FontTableBox": {
+               "@Size": "18",
+               "@Type": "ftab",
+               "@Specification": "3gpp",
+               "@Container": "tx3g text enct",
+               "FontRecord": {
+               "@ID": "1",
+               "@name": "Serif"
+               }
+           }
+           },
+    */
+
+    fn get_reference_mpeg_09_tx3g() -> Tx3g {
+        Tx3g {
+            data_reference_index: 1,
+            display_flags: 0x00040000,
+            horizontal_justification: 1,
+            vertical_justification: -1,
+            bg_color_rgba: RgbaColor {
+                red: 0xFF,
+                green: 0x00,
+                blue: 0x00,
+                alpha: 0xFF,
+            },
+            box_record: [0, 0, 60, 400],
+            style_record: [0, 0, 0, 0, 0, 1, 0, 18, 255, 255, 255, 255],
+            ftab: Some(Ftab {
+                font_entries: vec![FontEntry {
+                    font_id: 1,
+                    font: "Serif".into(),
+                }],
+            }),
+        }
+    }
+    #[test]
+    fn test_mpeg_09_text_decode() {
+        let buf: &mut std::io::Cursor<&&[u8]> = &mut std::io::Cursor::new(&ENCODED_TX3G_09);
+        let tx3g = Tx3g::decode(buf).expect("failed to decode tx3g");
+        assert_eq!(tx3g, get_reference_mpeg_09_tx3g());
+    }
+
+    #[test]
+    fn test_mpeg_09_text_encode() {
+        let tx3g = get_reference_mpeg_09_tx3g();
+        let mut buf = Vec::new();
+        tx3g.encode(&mut buf).unwrap();
+        assert_eq!(buf.as_slice(), ENCODED_TX3G_09);
     }
 }
