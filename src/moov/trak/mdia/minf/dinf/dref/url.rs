d1e9e923571cbbb42c049aec7f53cd70c6f3d8ff
Brad Hards <bradh@frogmouth.net>
2026-04-21 19:48:29 +1000

    dref: follow-up for self-contained flag fix, add tests (#150)

--- src/moov/trak/mdia/minf/dinf/dref/url.rs
+++ src/moov/trak/mdia/minf/dinf/dref/url.rs
@@ -35,8 +35,72 @@ impl AtomExt for Url {
 
         Ok(UrlExt {
             // ISOBMFF §8.7.2: flag bit 0 = media data is in the same file
-            self_contained: true,
+            self_contained: self.location.is_empty(),
             ..Default::default()
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const ENCODED_EMPTY: &[u8] = &[
+        0x00, 0x00, 0x00, 0x0c, b'u', b'r', b'l', b' ', 0x00, 0x00, 0x00, 0x01,
+    ];
+    #[test]
+    fn test_url_empty_encode() {
+        let url = Url {
+            location: "".into(),
+        };
+
+        let mut buf = Vec::new();
+        url.encode(&mut buf).unwrap();
+
+        assert_eq!(buf.as_slice(), ENCODED_EMPTY);
+    }
+
+    #[test]
+    fn test_url_empty_decode() {
+        let buf = &mut std::io::Cursor::new(&ENCODED_EMPTY);
+
+        let url = Url::decode(buf).expect("failed to decode url");
+
+        assert_eq!(
+            url,
+            Url {
+                location: "".into(),
+            }
+        );
+    }
+
+    const ENCODED_HTTP: &[u8] = &[
+        0x00, 0x00, 0x00, 0x2a, b'u', b'r', b'l', b' ', 0x00, 0x00, 0x00, 0x00, b'h', b't', b't',
+        b'p', b's', b':', b'/', b'/', b'e', b'x', b'a', b'm', b'p', b'l', b'e', b'.', b'c', b'o',
+        b'm', b'/', b'd', b'a', b't', b'a', b'.', b'b', b'l', b'o', b'b', 0,
+    ];
+    #[test]
+    fn test_url_http_encode() {
+        let url = Url {
+            location: "https://example.com/data.blob".into(),
+        };
+
+        let mut buf = Vec::new();
+        url.encode(&mut buf).unwrap();
+
+        assert_eq!(buf.as_slice(), ENCODED_HTTP);
+    }
+
+    #[test]
+    fn test_url_http_decode() {
+        let buf = &mut std::io::Cursor::new(&ENCODED_HTTP);
+
+        let url = Url::decode(buf).expect("failed to decode url");
+        assert_eq!(
+            url,
+            Url {
+                location: "https://example.com/data.blob".into(),
+            }
+        );
     }
 }
